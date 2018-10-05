f4a27a2a05dd5505580e85bc9e7542cbafe17472
alumno <alumno@itedes.BFACONSULTORA>
2018-10-04 21:50:22 -0300

    Ejercicios Web 1 y 2

--- modulo1/segmento2/apunte1/ejercicio1.web/ejercicio1.js
+++ modulo1/segmento2/apunte1/ejercicio1.web/ejercicio1.js
@@ -1,3 +1,9 @@
 function saludar(){
-	alert('hola mundo');
+	
+	const barrio = prompt('Ingrese su barrio');
+	const direccion = prompt('Ingrese la direccion de la comisaria mas cercana: ');
+	const entrecalle1 = prompt('Ingrese entrecalle1: ');
+	const entrecalle2 = prompt('Ingrese entrecalle2: ');
+	
+	alert(`La comisaria de  ${barrio} esta en la direccion ${direccion} entre la calle ${entrecalle1} y ${entrecalle2}`);
 }
---
+++ modulo1/segmento2/apunte1/ejercicio2.web/ejercicio2.js
@@ -0,0 +1,14 @@
+function saludar(){
+	
+	const dni = prompt('Ingrese su dni');
+	const nombre = prompt('Ingrese su nombre: ');
+	const apellido = prompt('Ingrese su apellido: ');
+	const direccion = prompt('Ingrese su direccion: ');
+	const telefono = prompt('Ingrese su telefono: ');
+	
+	alert(`DNI ${dni}`);
+	alert(`Nombre ${nombre}`);
+	alert(`Apellido ${apellido}`);
+	alert(`Direccion ${direccion}`);
+	alert(`Telefono ${telefono}`);
+}
