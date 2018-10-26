1376c25ff6435722585d5d3112e42901b6ca2d26
alumno <alumno@itedes.BFACONSULTORA>
2018-10-25 21:50:09 -0300

    apunte3

--- modulo1/segmento3/apunte3IfElse/ejercicio3.sh
+++ modulo1/segmento3/apunte3IfElse/ejercicio3.sh
@@ -6,9 +6,14 @@ read -p "Ingrese un caracter: " caracter
 if [[ "$caracter" == "a" || "$caracter"	== "b" || "$caracter" == "c" || "$caracter" == "o" || "$caracter" == "u" ]]
 then
 	opcion="vocal"
-elif [["$caracter" == "1" ||"$caracter" == "2" || "$caracter" == "3" || "$caracter" == "4" || "$caracter" == "5" || "$caracter" == "6" || "$caracter" == "7" || "$caracter" == "8" || "$caracter" == "9" || "$caracter" == "0"   ]]
+elif [[ "$caracter" == "1" || "$caracter" == "2" || "$caracter" == "3" || "$caracter" == "4" || "$caracter" == "5" || "$caracter" == "6" || "$caracter" == "7" || "$caracter" == "8" || "$caracter" == "9" || "$caracter" == "0"   ]]
 then
 	opcion="numero"
+elif [[ "$caracter" == "b" ||"$caracter" == "c" ||"$caracter" == "d" ||"$caracter" == "f" ||"$caracter" == "g" ||"$caracter" == "h" ||"$caracter" == "i" ||"$caracter" == "j" ||"$caracter" == "k" ||"$caracter" == "l" ||"$caracter" == "m" ||"$caracter" == "n" ||"$caracter" == "p" ||"$caracter" == "q" ||"$caracter" == "r" ||"$caracter" == "s" ||"$caracter" == "t" ||"$caracter" == "v" ||"$caracter" == "w" ||"$caracter" == "x" ||"$caracter" == "y" || "caracter" == "z" ]]
+then
+	opcion="consonante"
+else
+	opcion="error"
 fi
 echo $opcion
 
