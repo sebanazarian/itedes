c8a314effb3836d54556d7ae37cade947a0db9c8
alumno <alumno@itedes.BFACONSULTORA>
2019-01-08 21:02:18 -0300

    apunte 8

---
+++ modulo1/segmento3/apunte8EstrDeDatos/ejercicio2.net/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicio2.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> listaImpar = new List<int>();
+
+            for(int i=0;i<=100;i++)
+            {
+                if(i%2!=0)
+                {
+                    listaImpar.Add(i);
+                }
+            }
+
+            foreach(int nombre in listaImpar)
+            {
+                Console.WriteLine(nombre);
+            }
+        }
+    }
+}
