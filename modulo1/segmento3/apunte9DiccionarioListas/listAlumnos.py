899eaeb6dc89484fb08065bb0bc7d99d269fc84a
seba <sebanazarian@gmail.com>
2019-01-11 23:34:10 -0500

    apunte9 py

---
+++ modulo1/segmento3/apunte9DiccionarioListas/listAlumnos.py
@@ -0,0 +1,2 @@
+{'nombre': 'a', 'fechanac': 'a', 'direccion': '', 'apellido': 'a', 'dni': 'a', 'mail': 'a', 'telefono': 'a'}
+{'nombre': '2', 'fechanac': '2', 'direccion': '2', 'apellido': '2', 'dni': '2', 'mail': '2', 'telefono': '2'}
